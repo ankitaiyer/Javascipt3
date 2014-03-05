f1a984f190f1127f437605d5181a24080d6ecbc6
Student <user@chromebox-001.(none)>
2014-03-04 18:05:14 -0800

    jquery Ajax added for lists and items

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var formSubmitButton = $("#form_submit");
 
+    //to add list
     formSubmitButton.on("click", function(event){
         event.preventDefault();
         $.ajax({
@@ -16,14 +17,33 @@ $(document).ready(function(){
         });
     });
 
-    var getListsFromServer = function(){
-        $.ajax({
-            url: '/todo_lists/poll',
-            method: "GET"
-        }).done(function(data){
-            console.log(data);
-            $("#list-container").html(data);
-        });
-    };
-    setInterval(getListsFromServer, 10000);
+    // var getListsFromServer = function(){
+    //     $.ajax({
+    //         url: '/todo_lists/poll',
+    //         method: "GET"
+    //     }).done(function(data){
+    //         console.log(data);
+    //         $("#list-container").html(data);
+    //     });
+    // };
+    //setInterval(getListsFromServer, 10000);
+
+    //add list items
+    var formItemSubmitButton = $("#form_submit1");
+
+        formItemSubmitButton.on("click", function(event){
+            event.preventDefault();
+            $.ajax({
+                url: "/todo_lists/" + $("#todo-list-id").val(),
+                method: "POST",
+                data: $("form#todo-item-form").serialize(),
+            })
+            .done(function(data){
+               //alert(data);
+               $("#todo-list").html(data);
+            }).fail(function(){
+                alert('fail!!!');
+            });
+    });
+
 });

a3e582459c807801af8bc686805a6cd266350696
Student <user@chromebox-001.(none)>
2014-03-04 16:32:18 -0800

    AJAX for list name

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,29 @@
 $(document).ready(function(){
+    var formSubmitButton = $("#form_submit");
+
+    formSubmitButton.on("click", function(event){
+        event.preventDefault();
+        $.ajax({
+            url: "/",
+            method: "POST",
+            data: $("form#todo_list_form").serialize(),
+        })
+        .done(function(data){
+           //alert(data);
+           $("#list-container").html(data);
+        }).fail(function(){
+            alert('fail!!!');
+        });
+    });
+
+    var getListsFromServer = function(){
+        $.ajax({
+            url: '/todo_lists/poll',
+            method: "GET"
+        }).done(function(data){
+            console.log(data);
+            $("#list-container").html(data);
+        });
+    };
+    setInterval(getListsFromServer, 10000);
 });
